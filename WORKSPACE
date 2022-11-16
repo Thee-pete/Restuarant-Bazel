8901aa08c4392c51558f99410dfd4c8057ca93ec
Peter Ndumia <ndegwandumia@gmail.com>
2022-11-16 19:12:07 +0300

    Sample app working

--- WORKSPACE
+++ WORKSPACE
@@ -1,5 +1,4 @@
 android_sdk_repository(
     name = "androidsdk",
     api_level = 30,
-    build_tools_version = "30.0.3"
 )
--- src/main/java/com/pete/restaurant/MainActivity.java
+++ src/main/java/com/pete/restaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pete.restaurant;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
