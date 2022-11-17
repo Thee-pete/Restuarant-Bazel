4c1ad40b178711da84997a678ded65a49a75b2da
Peter Ndumia <ndegwandumia@gmail.com>
2022-11-17 09:53:23 +0300

    added viewmodel

--- src/main/java/com/pete/restaurant/MainActivityViewModel.java
+++ src/main/java/com/pete/restaurant/MainActivityViewModel.java
@@ -1,8 +1,25 @@
 package com.pete.restaurant;
 
 
+import android.app.Application;
 
+import java.util.List;
 
-public class MainActivityViewModel  {
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private RestaurantRepository restaurantRepository;
+    private final LiveData<List<Restaurant>> restaurants;
+
+    public MainActivityViewModel(Application application){
+        super(application);
+        restaurantRepository = new RestaurantRepository(application);
+        restaurants = restaurantRepository.getRestaurants();
+    }
+    LiveData<List<Restaurant>> getMyRestaurants(){
+        return restaurants;
+    }
+    public void insert(Restaurant restaurant){
+        restaurantRepository.insert(restaurant);
+    }
 
 }
