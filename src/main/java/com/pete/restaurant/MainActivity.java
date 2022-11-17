f600ce3de0e8b52fd68177ae1a1b73f3d9edc62b
Peter Ndumia <ndegwandumia@gmail.com>
2022-11-17 10:33:24 +0300

    added observer

--- src/main/java/com/pete/restaurant/MainActivity.java
+++ src/main/java/com/pete/restaurant/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    private MainActivityViewModel mainActivityViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,11 @@ public class MainActivity extends Activity {
         final RestaurantsAdapter adapter = new RestaurantsAdapter();
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        mainActivityViewModel.getMyRestaurants().observe(this, restaurants->{
+            adapter.submitList(restaurants);
+        });
 
 
 
