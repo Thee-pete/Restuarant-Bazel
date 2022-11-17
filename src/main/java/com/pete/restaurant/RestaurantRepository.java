0dc415eb3e1bfd67893fa86ad5bdf596431a67c8
Peter Ndumia <ndegwandumia@gmail.com>
2022-11-17 09:49:10 +0300

    added repository

---
+++ src/main/java/com/pete/restaurant/RestaurantRepository.java
@@ -0,0 +1,26 @@
+package com.pete.restaurant;
+
+import android.app.Application;
+
+import java.util.List;
+
+class RestaurantRepository {
+    private RestaurantDao restaurantDao;
+    private LiveData<List<Restaurant>> restaurants;
+
+    RestaurantRepository(Application application){
+        RestaurantDatabase restaurantDatabase = RestaurantDatabase.getInstance(application);
+        restaurantDao = restaurantDatabase.restaurantDao();
+        restaurants = restaurantDao.getAllRestaurants();
+    }
+
+    LiveData<List<Restaurant>> getRestaurants(){
+        return restaurants;
+    }
+
+    void insert(Restaurant restaurant){
+        RestaurantDatabase.databaseWriteExecutor.execute(()->{
+            restaurantDao.insert(restaurant);
+        });
+    }
+}
